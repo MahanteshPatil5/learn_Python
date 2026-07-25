56cd39d294d3f4fb0d694a18cae5034fed67187d
MahanteshPatil5 <mantup506@gmail.com>
2026-07-25 23:45:27 +0530

    basics

--- stage1/first.py
+++ stage1/first.py
@@ -1,1 +1,16 @@
 print("Hello git")
+
+# Variables
+name = "Mahantesh"
+age = 20
+
+# Input
+city = input("Enter city: ")
+
+# Processing
+message = "Hello " + name
+
+# Output
+print(message)
+print(age)
+print(city)
