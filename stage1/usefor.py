738f3c81e45147fbe1a7b5e61e2d47f9180c56c8
MahanteshPatil5 <mantup506@gmail.com>
2026-07-27 00:07:16 +0530

    added loop

--- stage1/usefor.py
+++ stage1/usefor.py
@@ -5,4 +5,43 @@ for i in range(len(name)):
 # or use
 
 for ch in name:
-    print(ch)
+    print(ch)  # use when no need of index
+    
+# next 
+squre_of_num = []
+for i in range(5):
+    squre_of_num.append(i*i)
+
+# use 
+squre_of_num = [i*i for i in range(5)]
+
+
+# while looping
+password = ""
+while password != "admin":
+    password =input("Enter the password")
+    
+# use
+# while condition:
+    # work
+    
+
+# question
+
+# Print numbers from 1 to 10.
+for i in range(100):
+    print(i)
+    
+    
+# Print the multiplication table of 5.
+for i in range(10):
+    print(i/2);
+
+Print the sum of numbers from 1 to 10.
+Print each character of a string.
+while
+Print numbers from 10 to 1.
+Keep asking the user for a password until they enter "admin".
+for i in range(100):
+    print(i)
+    
