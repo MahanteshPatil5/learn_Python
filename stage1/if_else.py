a09018ecaaf14d785b3d807f5ef6905cc45e4099
MahanteshPatil5 <mantup506@gmail.com>
2026-07-27 00:01:05 +0530

    added loop

--- stage1/if_else.py
+++ stage1/if_else.py
@@ -24,3 +24,20 @@ if marks>35 and attendence>85:
 #     print("use below way");
 # # use 
 # if fruit in ["apple",'banana',"orange"]:
+
+# Question 
+# Check whether a number is positive or negative.
+num = float(input("enter a number : "))
+print("number negative") if num<0 else print("number is positive")
+
+
+# Check whether a number is even or odd.''
+print("even ") if num%2==0 else print("odd")
+
+# Find the larger of two numbers.
+n1 ,n2 = 10,5
+large = n1 if n1>n2 else n2
+
+# Check whether a person is eligible to vote (18+).
+age = input("Enter the age : ")
+print("older eligble") if age>=18 else print(" not eligible") 
---
+++ stage1/usefor.py
@@ -0,0 +1,8 @@
+name = "python"
+for i in range(len(name)):
+    print(name[i])
+
+# or use
+
+for ch in name:
+    print(ch)
