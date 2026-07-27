49e81877894786c2cb227fed8175419ea4589c8e
MahanteshPatil5 <mantup506@gmail.com>
2026-07-28 00:45:12 +0530

    tuples concept

---
+++ stage1/tuple.py
@@ -0,0 +1,14 @@
+# tuple
+# similar to list but once created not allowed to chnage the data present 
+# just similar to list use () save like months,days,RGB color values
+
+days = ("mon", "tue","wed","thu","fri","sat")
+# access using index
+
+
+
+# Question
+# Create a tuple of the seven weekdays and print the last day.
+days = ("sun","mon", "tue","wed","thu","fri","sat")
+print(days[6])
+# use neagtive so looks good print(days[-1])
