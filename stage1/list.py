1fb0565794a58fb03e7094399bffa4f6c55ce354
MahanteshPatil5 <mantup506@gmail.com>
2026-07-28 00:43:46 +0530

    questions

--- stage1/list.py
+++ stage1/list.py
@@ -1,4 +1,12 @@
 # list
 #  A list is a collection of values stored in a single variable.
-
+# Lists are ordered, so the position of each element matters.
+# Lists are also mutable, which means you can add, remove, or change elements after creating the list.
 student = ["mahantesh","vivek","vinay"]
+
+# list of number from 1 to 10
+num = list(range(1,11))
+
+# list can different data types data in single variable (tagether we can save different data)
+list = [10,"name",10.333,True]
+
