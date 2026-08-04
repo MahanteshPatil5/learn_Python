3f1f3d5fcacd1e3455086294f062be4a31066dbd
MahanteshPatil5 <mantup506@gmail.com>
2026-08-05 00:48:59 +0530

    learn module

--- stage2/module.py
+++ stage2/module.py
@@ -14,8 +14,35 @@ import math
 print(math.sqrt(25))
 
 # use
-
-from math import sqrt
+from math import sqrt #imported sqrt from math so no need of using mant.sqrt
 print(sqrt(36))
 
-from math import factorial, sqrt
+
+#example  from math import factorial, sqrt 
+
+# alising other concept
+# use as  
+
+import numpy as np # use as to short the word numpy to np
+# use 
+np.array()
+
+#example import pandas as pd
+
+# dir() function used to find what is inside module 
+
+# 3thsis a aiw red 
+
+# math module
+# call  use from should,import math,naivile
+math.factorial(9)
+math.ceil(3.2)  #upword value
+math.floor(3.2) #down word value  we can use some other too 
+
+
+# random module 
+# a simple module which jave similar ouput as once thw questiion
+# use 
+import random
+random.randinit(1,10)
+random.choice(arr()) #usefull and find full null value evrytimr this is called
