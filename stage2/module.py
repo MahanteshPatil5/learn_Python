053ac0d236fefb2d2c96bc53eb9743b156347055
MahanteshPatil5 <mantup506@gmail.com>
2026-08-05 23:41:18 +0530

    changes

--- stage2/module.py
+++ stage2/module.py
@@ -31,18 +31,32 @@ np.array()
 
 # dir() function used to find what is inside module 
 
-# 3thsis a aiw red 
-
-# math module
+# 1. math module
 # call  use from should,import math,naivile
 math.factorial(9)
-math.ceil(3.2)  #upword value
+math.ceil(3.2) #upword value ceil value of the number
 math.floor(3.2) #down word value  we can use some other too 
+math.pi # built in pi value
 
+# 2. random module 
+# a module which is used for random values : generate ramdon things
 
-# random module 
-# a simple module which jave similar ouput as once thw questiion
 # use 
-import random
-random.randinit(1,10)
-random.choice(arr()) #usefull and find full null value evrytimr this is called
+import random #module random
+random.randinit(1,10)  # this is used find the random value in the range 1 to 10
+random.choice(list)   #used for finding a random choice of list 
+random.shuffle(list) #this is used to shuffle the list randomly
+
+# 3.Data time  used to find the date and time
+# used in login page, attendence, file creation, backend api
+from datetime import datetime
+print(datetime.now())
+
+# 4. json   
+# module used for data exchange between frontend and backend 
+# a dictionay 
+student = {"name":"mahantesh", "age": 20}
+import json
+json.dumps(student) #dictionary to json 
+json.loads(student) #json to dictionary
+
