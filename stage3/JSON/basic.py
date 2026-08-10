5a5c1f9f5987623a0a4b441a4ec419a813835ad7
MahanteshPatil5 <mantup506@gmail.com>
2026-08-10 23:54:15 +0530

    learnt basic of json

--- stage3/JSON/basic.py
+++ stage3/JSON/basic.py
@@ -32,3 +32,45 @@ print(data)             # {"name" : "mahantesh", "age":20}
 data2 = '{"name":"Mantu", "cgpa": 8.4}'
 stud = json.loads(data2)
 print(stud)             #{'name': 'Mantu', 'cgpa': 8.4}
+
+# dumps(), dump(), loads(),load()
+
+# dumps() ::  python object to json string 
+# dump()  ::  python obkect to json file 
+# loads() ::  json sting to python object
+# load()  ::  json file to pyhton object
+
+# JSON file example
+# we have student.json (file)
+# import json
+student = {
+    "name" : "Crazy",
+    "age" : 22,
+    "cgpa" : 8.4,
+    "branch" : "CSE"
+}
+
+with open("student.json", "w") as file:
+    json.dump(student,file)
+    print("file created with name :  student.json")
+with open("student.json", "r") as file:
+    student = json.load(file)
+print(student["name"],student["age"])
+
+
+# always key in JSOn must be  strings
+
+# questions
+# Create a Python dictionary containing your name, age, branch, and skills. Convert it into a JSON string using dumps().
+mydata = {
+    "name" : "mahantesh", "age":20, "branch" : "CSE", "skills" : "FE,python,sql,C++"
+}
+
+# Take a JSON string and convert it back into a Python dictionary using loads().
+with open("mydata.json","w") as myfile:
+    json.dump(mydata,myfile)
+with open("mydata.json","r") as myfile:
+    mydata = json.load(myfile)
+    
+print("my data : ",mydata)
+# Create student.json, store student information in it using dump(), then read it using load().
