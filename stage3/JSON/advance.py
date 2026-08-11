35b49a25fe3a5b0303b6b9c57b0fac66eb6d006f
MahanteshPatil5 <mantup506@gmail.com>
2026-08-11 23:23:01 +0530

    advance json file

---
+++ stage3/JSON/advance.py
@@ -0,0 +1,10 @@
+# 1.nested JSON  data is nested
+data = {
+    "id" : 94,"name":"Mantu","address":{"city":"Gokak","pin": 591224 },
+    "skill":["C","C++","python"]
+}
+
+data["address"]["city"] # Gokak
+
+
+# 2.JOSM Sr9cals ptjrt onkect 
