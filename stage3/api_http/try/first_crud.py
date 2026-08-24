cef7ba8699cff8f476a30e81bdb3145eb20205a0
MahanteshPatil5 <mantup506@gmail.com>
2026-08-24 14:38:05 +0530

    added crud file

---
+++ stage3/api_http/try/first_crud.py
@@ -0,0 +1,37 @@
+from flask import Flask,request
+app = Flask(__name__)
+students = [] # empty list
+
+#create::
+@app.route("/students",methods=["POST"])
+def create_student():
+    data = request.get_json()
+    student = {
+        "id":len(students)+1,
+        "name":data["name"],
+        "branch":data["branch"]
+    }
+    students.append(student)
+    return student,201
+
+#read all
+@app.route("/students/<int :id>",method= ["GET"])
+def get_student(id):
+    for student in students:
+        if(student["id"] == id):
+            return student,200
+        
+    return {"error" : " student not found"},404
+
+#patch:
+@app.route("/students/<int:id>", methods = ["patch"])
+def update():
+    data = request.get_json()
+    for student in students:
+        if(student["id"]==id):
+            if "name" in data:
+                student["name"] = data["name"]
+            if "branch" in data:
+                student["branch"]==data["branchs"]
+            return students.200
+        return {"messaages":"studen not fornd;"}
