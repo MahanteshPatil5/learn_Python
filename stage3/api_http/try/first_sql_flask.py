66faf8218d528b1646cbc02b2357b0821e4960cf
MahanteshPatil5 <mantup506@gmail.com>
2026-09-01 00:21:23 +0530

    working sql_flask

--- stage3/api_http/try/first_sql_flask.py
+++ stage3/api_http/try/first_sql_flask.py
@@ -1,3 +1,31 @@
 from flask import Flask,jsonify
-import _mysql_connector
+import mysql.connector
 app = Flask(__name__)
+
+db = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "Svce@2024",
+    database = "LEARN_DB"
+)
+cursor = db.cursor()
+
+@app.route("/")
+def home():
+    return "this is the home page"
+
+@app.route("/students", methods= ["GET"])
+def get_students():
+    cursor.execute("select * from students")
+    rows = cursor.fetchall()
+    students = []
+    for row in rows:
+        students.append({
+            "id" :row[0],
+            "name":row[1],
+            "branch":row[2]
+        })
+    return students
+        
+
+app.run(debug=True)
