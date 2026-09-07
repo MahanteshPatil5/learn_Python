de505cb7cf382a2b3fcf0560638d938b48800c71
MahanteshPatil5 <mantup506@gmail.com>
2026-09-07 23:56:39 +0530

    added put,patch

--- stage3/api_http/try/first_sql_flask.py
+++ stage3/api_http/try/first_sql_flask.py
@@ -30,16 +30,64 @@ def get_students():
 @app.route("/students", methods= ["POST"])
 def post_students():
     
-    data = request.get_json({
-    "name": "Vivek",
-    "branch": "MBBS"
-})
+    data = request.get_json()
     
     if not data or "name" not in data or "branch" not in data:
         return "400 error bro enter proper data", 400
-    cursor.execute("INSERT INTO STUDENTS VALUES (%s,%s)",("name", "branch"))
+    
+    cursor.execute("INSERT INTO students (name,branch) VALUES (%s,%s)",(data["name"],data["branch"]))
     db.commit()
-    last = cursor.lastrowid()
-    return {"message" : "student created successfully ","id" = last}
+    lastid = cursor.lastrowid
+    return {"message" : "student created successfully ", "id" : lastid},201
 
+
+@app.route("/students/<int:id>",methods=["PUT"])
+def put_student(id):
+    data = request.get_json()
+    name = data["name"]
+    branch = data["branch"]
+    
+    cursor.execute(
+        "UPDATE students SET name=%s, branch=%s WHERE id= %s",(name,branch,id) 
+    )
+    db.commit()
+    if cursor.rowcount == 0:
+        return {"error":"student not found"},404
+    
+    return{
+        "message":"student updated successfully"
+    },200
+
+@app.route("/students/<int:id>",methods=["PATCH"])
+def patch_student(id):
+    data = request.get_json()
+    
+    if "name" in data:
+        cursor.execute(
+            "UPDATE students SET name=%s WHERE id=%s",(data["name"],id)
+        )
+    
+    if "branch" in data:
+        cursor.execute(
+            "UPDATE students SET branch=%s WHERE id=%s",(data["branch"],id)
+        )
+        
+    db.commit()
+    return{
+        "message":"student partially updated"
+    },200
+    
+
+@app.route("/students/<int:id>",methods=["DELETE"])
+def delet_student(id):
+    cursor.execute(
+        "DELETE FROM students WHERE id =%s",(id,)
+    )
+    db.commit()
+    if cursor.rowcount == 0:
+        return {"error": "Student not found"}, 404
+    return {
+        "message":"student deleted successfully "
+    },200
+    
 app.run(debug=True)
--- stage3/api_http/try/sql_post_reqst.py
+++ stage3/api_http/try/sql_post_reqst.py
@@ -1,7 +1,47 @@
 import requests
-url = "http://127.0.0.1:5000/students"
-data = {
-    "name":"Vivek","branch":"MBBS"
+# url = "http://127.0.0.1:5000/students"
+# data = {
+#     "name":"Vivek","branch":"MBBS"
+# }
+# response = requests.post(url,json = data)
+# print(response.json())
+
+# put
+puturl = "http://127.0.0.1:5000/students/15"
+putdata = {
+    "name" : "VINAY Put chnged","branch":"AI"
 }
-response = requests.post(url,json = data)
+resput = requests.put(puturl,json=putdata)
+if resput.status_code==200:
+    print("Chnged using put: ",resput.json())
+else:
+    print("Failed to change data ")
+    
+
+patchurl = "http://127.0.0.1:5000/students/16"
+patchdata = {
+    "branch":"MBBS MD updated patch" #updating only branch  
+}
+res = requests.patch(patchurl,json=patchdata)
+
+if res.status_code==200:
+    print("Chnged using patch: ",res.json())
+else:
+    print("Failed to change data ")
+    
+posturl = "http://127.0.0.1:5000/students"
+data = {
+    "name":"Vandana","branch":"CSE engineer"
+}
+response = requests.post(posturl,json = data)
 print(response.json())
+
+
+delete_url = "http://127.0.0.1:5000/students/20"
+res_delete = requests.delete(delete_url)
+if res_delete.status_code==200:
+    print("Delete using delete :: ")
+else:
+    print("Failed to delete")
+
+
