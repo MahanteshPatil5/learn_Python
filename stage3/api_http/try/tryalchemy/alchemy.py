abfb943a45d7a821dd238b681a61aacb44ae39f7
MahanteshPatil5 <mantup506@gmail.com>
2026-09-16 23:55:22 +0530

    added content

--- stage3/api_http/try/tryalchemy/alchemy.py
+++ stage3/api_http/try/tryalchemy/alchemy.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask,jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABSE_URL"] = "mysql+mysqlconnector://DB_USER:DB_PASSWORD@DB_HOST/DB_NAME"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+mysqlconnector://root:Svce%402024@localhost/LEARN_DB"
 
 db = SQLAlchemy(app)
 
@@ -11,17 +11,46 @@ db = SQLAlchemy(app)
 class Teacher(db.Model):
     __tablename__ = "teachers"
     
-    id = db.column(db.Integer, primary_key=True)
-    name = db.column(db.string(20),nullable = False)
-    subject = db.column(db.string(20),nullable = False)
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20),nullable = False)
+    subject = db.Column(db.String(20),nullable = False)
     
 #add a teacher
-teacher = Teacher(
-    nmae = "Geeta",subject = "Home"
-)
-db.session.add(teacher)
-db.session.commit()
+with app.app_context(): 
+    # create new table
+    db.create_all()    
+    # teacher = Teacher(
+    # name = "Geeta",subject = "Home"
+    # )
+    # db.session.add(teacher)
+    # db.session.commit()
 
 # read datat in table
 
-teacher = Teacher.query.all()
+# teacher = Teacher.query.all()
+
+@app.route("/")
+def home():
+    return "u r in home"
+
+@app.route("/teacher",methods=["GET"])
+def teachers_data():
+    teachers = Teacher.query.all()
+
+    if not teachers:
+        return jsonify({"message": "no teachers found in databse"}),404
+    
+    teacher_list = []
+    for teacher in teachers:
+        teacher_list.append({
+            "id": teacher.id,
+            "name":teacher.name,
+            "subject":teacher.subject
+        })
+    return jsonify(teacher_list),200
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
